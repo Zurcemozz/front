2d02bf9327050a230f35a5f039a6865d7d6fd778
Zurcemozz <zurcemozz@gmail.com>
2022-04-05 00:39:28 +0800

    Updates

---
+++ precache-manifest.d74ecbb12ecceafb2945f7c156085e71.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd441b38e17506336f9d2f55b6b4e9ff",
+    "url": "/client/index.html"
+  },
+  {
+    "revision": "7a0ecdb8f98c9f688e60",
+    "url": "/client/static/css/main.25336470.chunk.css"
+  },
+  {
+    "revision": "bf2f098a3441313e5e69",
+    "url": "/client/static/js/2.e494c28c.chunk.js"
+  },
+  {
+    "revision": "6f001b22a9cb1fcb0d750f6c56d216df",
+    "url": "/client/static/js/2.e494c28c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a0ecdb8f98c9f688e60",
+    "url": "/client/static/js/main.641c941c.chunk.js"
+  },
+  {
+    "revision": "6e28ab8ea61b90f39a49",
+    "url": "/client/static/js/runtime-main.c730adb0.js"
+  },
+  {
+    "revision": "4b9dc077a5e83bc17478662d0144dc92",
+    "url": "/client/static/media/memories-Logo.4b9dc077.png"
+  },
+  {
+    "revision": "18ebdb6d48b70ab2e72ac3bf9154d8be",
+    "url": "/client/static/media/memories-Text.18ebdb6d.png"
+  }
+]);
